523aed71377831e2408a5200bf8717e4b93042f6
Karina Widhia Nugrahini <karinwidhia@gmail.com>
2017-03-22 19:56:15 +0700

    Layout tambah belum fix

--- app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl11517192933/costin/TambahFragment.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl11517192933/costin/TambahFragment.java
@@ -1,11 +1,20 @@
 package id.sch.smktelkom_mlg.project2.xirpl11517192933.costin;
 
 
+import android.app.DatePickerDialog;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 
 /**
@@ -13,6 +22,11 @@ import android.view.ViewGroup;
  */
 public class TambahFragment extends Fragment {
 
+    TextView etTgl;
+    int mYear;
+    int mMonth;
+    int mDay;
+    Spinner spJenis;
 
     public TambahFragment() {
         // Required empty public constructor
@@ -22,8 +36,32 @@ public class TambahFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.tambah_layout, container, false);
+
+        spJenis = (Spinner) v.findViewById(R.id.spinnerJenis);
+
+        etTgl = (EditText) v.findViewById(R.id.editTexttgl);
+
+        etTgl.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+                Calendar mcurrentDate = Calendar.getInstance();
+                mYear = mcurrentDate.get(Calendar.YEAR);
+                mMonth = mcurrentDate.get(Calendar.MONTH);
+                mDay = mcurrentDate.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog mDatePicker = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                    public void onDateSet(DatePicker datePicker, int selectedyear, int selectedmonth, int selectedday) {
+
+                    }
+                }, mYear, mMonth, mDay);
+                mDatePicker.setTitle("Select date");
+                mDatePicker.show();
+            }
+        });
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.tambah_layout, container, false);
     }
-
 }
