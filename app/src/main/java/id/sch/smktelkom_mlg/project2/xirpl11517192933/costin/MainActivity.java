c35e15790436f5939aea446663a98c198520fbd9
Miftahul Rohmah <miftahul_rohmah_24rpl@student.smktelkom-mlg.sch.id>
2017-03-24 20:27:44 +0700

    Mifta: HelpFragment & AboutFragment

--- app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl11517192933/costin/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl11517192933/costin/MainActivity.java
@@ -81,9 +81,7 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-        /**
-         * Setup Drawer Toggle of the Toolbar
-         */
+
 
         android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
         ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.app_name,
